a626fa905df3e284c018b4eaa0e3cf2b058c45c1
LucasT77 <74610299+LucasT77@users.noreply.github.com>
2023-01-25 18:20:08 +0000

    Add files via upload

--- push_swap3/radix.c
+++ push_swap3/radix.c
@@ -6,7 +6,7 @@
 /*   By: luaraujo <luaraujo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/21 20:51:29 by luaraujo          #+#    #+#             */
-/*   Updated: 2023/01/24 16:58:37 by luaraujo         ###   ########.fr       */
+/*   Updated: 2023/01/25 18:19:11 by luaraujo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ void	print_array_binary(int *array, int size)
 	}
 }
 
-static void	radix_aux1(int **stack_a, int **stack_b, int **sizes, int *index)
+/*static void	radix_aux1(int **stack_a, int **stack_b, int **sizes, int *index)
 {
 	selector(&(*stack_a), &(*stack_b), &(*sizes), "sa");
 	selector(&(*stack_a), &(*stack_b), &(*sizes), "pb");
@@ -74,7 +74,7 @@ static void	radix_aux3(int **stack_a, int **stack_b, int **sizes, int *index)
 	}
 	selector(&(*stack_a), &(*stack_b), &(*sizes), "pb");
 	*index = 0;
-}
+}*/
 
 static void	radix_aux(int **stack_a, int **stack_b, int **sizes, int bit)
 {
@@ -85,7 +85,7 @@ static void	radix_aux(int **stack_a, int **stack_b, int **sizes, int bit)
 	middle = (*sizes)[0] / 2;
 	while (index < (*sizes)[0])
 	{
-		if (((*stack_a)[index] >> bit & 1) == 0 && index == 0)
+		/*if (((*stack_a)[index] >> bit & 1) == 0 && index == 0)
 			selector(&(*stack_a), &(*stack_b), &(*sizes), "pb");
 		else if (((*stack_a)[index] >> bit & 1) == 0 && index == 1)
 			radix_aux1(&(*stack_a), &(*stack_b), &(*sizes), &index);
@@ -94,7 +94,17 @@ static void	radix_aux(int **stack_a, int **stack_b, int **sizes, int bit)
 		else if (((*stack_a)[index] >> bit & 1) == 0 && index > middle)
 			radix_aux3(&(*stack_a), &(*stack_b), &(*sizes), &index);
 		else
+			index++;*/
+		if (((*stack_a)[index] >> bit & 1) == 0)
+		{
+			selector(&(*stack_a), &(*stack_b), &(*sizes), "pb");
+			index = 0;
+		}
+		else
+		{
+			selector(&(*stack_a), &(*stack_b), &(*sizes), "ra");
 			index++;
+		}
 	}
 }
 
@@ -109,10 +119,10 @@ void	radix(int **stack_a, int **stack_b, int **sizes)
 	while (bit < 32)
 	{
 		radix_aux(&(*stack_a), &(*stack_b), &(*sizes), bit);
-		/*ft_printf("stack_a:\n");
+		ft_printf("stack_a:\n");
 		print_array_binary((*stack_a), (*sizes)[0]);
 		ft_printf("\nstack_b:\n");
-		print_array_binary((*stack_b), (*sizes)[1]);*/
+		print_array_binary((*stack_b), (*sizes)[1]);
 		//if ((*sizes)[0] <= 5)
 		//	sort_for5(&(*stack_a), &(*stack_b), &(*sizes));
 		i = (*sizes)[1];
